dfdcdff1084234e8812d321d835de3d6ea64cd06
olim <olim530102@gmail.com>
2025-01-26 17:38:14 +0500

    add carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,12 @@
-// 'use strict';
-// let newCurrentUser = "John Doe";
-// let ourPlanetName = "Earth";
-// let ageUser = 30;
-// let confirmUser = true;
-
-// // console.log(`User: ${newCurrentUser}, Planet: ${ourPlanetName}, Age: ${ageUser}`); // User: John Doe, Planet: Earth, Age: 30
-// // console.log("User: " + newCurrentUser + ", Planet: " + ourPlanetName + ", Age: " + ageUser);
-// // console.log("Age: " + (ageUser + 50 / 2 )); // Age: 55
-
-// // ageUser = 40;
-// // console.log("Age: " + (ageUser + 50 / 2 )); // Age: 60
-
-// // alert("User Name: " + newCurrentUser + ", Planet: " + ourPlanetName + ", Age: " + ageUser);
-
-// // ageUser = prompt("Enter your age: ", ageUser);
-// // alert("Your age is: " + ageUser);
-// // confirmUser = confirm("Are you sure your age is:" + ageUser);
-// // alert("You have confirmed your age: " + confirmUser);
-
-// ageUser = prompt("сколько вам лет: ", ageUser);
-// typeof ageUser;
-
-// let value = false;
-// alert(typeof value);
-// value = String(value);
-// alert(typeof value);
-
-let str =  "dsfsdf";
-console.log(str);
-str = Number(str);
-console.log(str);
-
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',  // Горизонтальный или вертикальный слайдер
+    loop: true,               // Бесконечная прокрутка
+    pagination: {
+        el: '.swiper-pagination', // Элемент для точек навигации
+        clickable: true,         // Точки можно кликать
+    },
+    navigation: {
+        nextEl: '.swiper-button-next', // Кнопка "вперёд"
+        prevEl: '.swiper-button-prev', // Кнопка "назад"
+    },
+});
