7537117b302168e123bcf42c7957fd8db2344770
olim <olim530102@gmail.com>
2025-02-02 22:24:11 +0500

    add catalog

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,20 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev', // Кнопка "назад"
     },
 });
+
+function toggleSlide(item) {
+    document.querySelectorAll(item).forEach((element, i) => {
+        element.addEventListener('click', function(e) {
+            e.preventDefault();
+            // Получаем соответствующие элементы по индексу
+            const contents = document.querySelectorAll('.catalog-item__content');
+            const lists = document.querySelectorAll('.catalog-item__list');
+            // Переключаем классы
+            contents[i].classList.toggle('catalog-item__content--active');
+            lists[i].classList.toggle('catalog-item__list--active');
+        });
+    });
+}
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
